524683cdc4d3b9356d20d40438659cc426f7efb1
stormdeveloper-glitch <stormdev.coder@gmail.com>
2026-03-17 23:30:33 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID", 0))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+SESSION_STRING = os.getenv("SESSION_STRING", "")
+ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+# Kanallarni vergul bilan ajratib yozing: "@kanal1,@kanal2"
+CHANNELS = os.getenv("CHANNELS", "").split(",")

c75f7fd00bab4e5a1704d989c83d78b4eb1af7fe
stormdeveloper-glitch <stormdev.coder@gmail.com>
2026-03-18 17:02:34 +0500

    Change ADMIN_ID to a list and add CHANNELS

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ API_ID = int(os.getenv("API_ID", 0))
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 SESSION_STRING = os.getenv("SESSION_STRING", "")
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+ADMIN_ID = [int(x) for x in os.getenv("ADMIN_ID", "0").split(",")]
 # Kanallarni vergul bilan ajratib yozing: "@kanal1,@kanal2"
 CHANNELS = os.getenv("CHANNELS", "").split(",")
